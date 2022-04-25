492a1a9f2e07058af97dcb2cd522f7a759953619
JeremyLudwigDev <jeremy.ludwig@shopify.com>
2022-04-25 14:35:16 -0400

    Incorporate PR feedback

--- polaris-react/src/components/DataTable/DataTable.tsx
+++ polaris-react/src/components/DataTable/DataTable.tsx
@@ -529,7 +529,7 @@ class DataTableInner extends PureComponent<CombinedProps, DataTableState> {
     }
   };
 
-  private onHover = (row: number) => {
+  private handleHover = (row?: number) => () => {
     this.setState({rowHovered: row});
   };
 
